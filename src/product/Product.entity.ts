32cd7ebaacf556ecad4fdfb9283fda9cfa49f3bd
GabrielEvangelista06 <gabrielevangelista094@gmail.com>
2023-06-06 19:33:55 -0300

    Adaptando a entidade Product para funcionar com o TypeORM

--- src/product/Product.entity.ts
+++ src/product/Product.entity.ts
@@ -1,16 +1,44 @@
-import { ProductCharacteristicsDTO } from './dto/ProductCharacteristics.dto';
-import { ProductImageDTO } from './dto/ProductImage.dto';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
+@Entity({ name: 'products' })
 export class ProductEntity {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ name: 'userId', length: 100, nullable: false })
   userId: string;
+
+  @Column({ name: 'name', length: 100, nullable: false })
   name: string;
+
+  @Column({ name: 'price', nullable: false })
   price: number;
+
+  @Column({ name: 'availableQuantity', nullable: false })
   availableQuantity: number;
+
+  @Column({ name: 'description', length: 255, nullable: false })
   description: string;
-  characteristics: ProductCharacteristicsDTO[];
-  images: ProductImageDTO;
+
+  // characteristics: ProductCharacteristicsDTO[];
+  // images: ProductImageDTO;
+
+  @Column({ name: 'category', length: 100, nullable: false })
   category: string;
-  crated_at: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  created_at: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
   updated_at: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deleted_at: string;
 }
