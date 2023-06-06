fb97e05c2618d3ee965c76b80150c82bc6cf994d
GabrielEvangelista06 <gabrielevangelista094@gmail.com>
2023-06-06 08:04:50 -0300

    Alterando a forma de como é retornado o Usuário nas requisições

--- src/user/User.controller.ts
+++ src/user/User.controller.ts
@@ -1,6 +1,9 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+import { UserEntity } from './User.entity';
 import { UserRepository } from './User.repository';
 import { CreateUserDTO } from './dto/CreateUser.dto';
+import { ListUserDTO } from './dto/ListUser.dto';
 
 @Controller('/users')
 export class UserController {
@@ -8,13 +11,28 @@ export class UserController {
 
   @Post()
   async createUser(@Body() userData: CreateUserDTO) {
-    this.userRepository.save(userData);
+    const user = new UserEntity();
 
-    return userData;
+    user.id = uuid();
+    user.name = userData.name;
+    user.email = userData.email;
+    user.password = userData.password;
+
+    this.userRepository.save(user);
+
+    return {
+      status: 'ok',
+      message: 'Usuário criado com sucesso!',
+      user: new ListUserDTO(user.id, user.name),
+    };
   }
 
   @Get()
   async listUsers() {
-    return this.userRepository.list();
+    const savedUsers = await this.userRepository.list();
+
+    const users = savedUsers.map((user) => new ListUserDTO(user.id, user.name));
+
+    return users;
   }
 }
