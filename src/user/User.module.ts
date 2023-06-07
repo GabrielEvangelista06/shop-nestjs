40e32990417a738db27e739a118a367abfd58b6a
GabrielEvangelista06 <gabrielevangelista094@gmail.com>
2023-06-07 08:37:22 -0300

    Removendo UserRepository

--- src/user/User.module.ts
+++ src/user/User.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './User.controller';
 import { UserEntity } from './User.entity';
-import { UserRepository } from './User.repository';
 import { UserService } from './User.service';
 import { EmailIsUniqueValidator } from './validator/email-is-unique.validator';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
-  providers: [UserRepository, EmailIsUniqueValidator, UserService],
+  providers: [EmailIsUniqueValidator, UserService],
 })
 export class UserModule {}
